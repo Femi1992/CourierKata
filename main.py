38c55a865ad08073eb8d2f918fc427dc24938a48
Femi1992 <olufemidokun@hotmail.co.uk>
2021-05-06 10:00:26 +0100

    updated test parcel size method

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ def getParcelSize():
     parcelDimTotal = sum(map(int, stringParcelDim))
 
     if parcelDimTotal < 10:
-        print("Small Parcel: $3. Total Cost $3")
+        result = ("Small Parcel: $3. Total Cost $3")
     elif parcelDimTotal < 50 and parcelDimTotal > 10:
-        print("Medium Parcel: $8. Total Cost $8")
+        result = ("Medium Parcel: $8. Total Cost $8")
     elif parcelDimTotal < 100 and parcelDimTotal > 50:
-        print("Large Parcel: $15. Total Cost $15")
+        result = ("Large Parcel: $15. Total Cost $15")
     else:
-        print("Extra Large Parcel: $25. Total Cost $25")
+        result = ("Extra Large Parcel: $25. Total Cost $25")
+    return result
 getParcelSize()
--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
 import unittest
+import main
 
 class TestParcelPrices(unittest.TestCase):
 
     def testParcelSizePrice(self):
-        self.assertEqual("1cmx1cmx1cmx", "Small Parcel: $3. Total Cost: $3")
-        self.assertFalse("1cmx1cmx1cmx", "Small Parcel: $3. Total Cost: $3")
+        result = main.getParcelSize()
+        self.assertEqual(result, "Small Parcel: $3. Total Cost $3")
 
 if __name__ == '__main__':
-    test = TestParcelPrices()
-    test.testParcelSizePrice()
+    unittest.main()
